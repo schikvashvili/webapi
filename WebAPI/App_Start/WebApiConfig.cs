6263ef512b8605363bc8a68eb04a4f03669a5246
talknshare.info@gmail.com <talknshare.info@gmail.com>
2019-01-23 10:02:29 +0200

    commit first webapi

---
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static string UrlPrefix { get { return "api"; } }
+        public static string UrlPrefixRelative { get { return "~/api"; } }
+        public static void Register(HttpConfiguration config)
+        {
+              
+            //enable cors
+            //config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*"));
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: UrlPrefix + "/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            config.Formatters.JsonFormatter.SerializerSettings = new Newtonsoft.Json.JsonSerializerSettings();
+        }
+    }
+}
---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors("http://localhost:4200", "*", "*",PreflightMaxAge =100000,SupportsCredentials =true)]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            var session = System.Web.HttpContext.Current.Session;
+
+            if (session != null)
+            {                
+                if (session["Values"] == null)
+                {
+                    session["Values"] = new string[] { "value1", "value2", "value3" };
+                    return new string[] { "value1" };
+                }
+            }
+
+            return new string[] { JsonConvert.SerializeObject((Object)session["Values"]) };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IEnumerable<string> Post([FromBody]LoginContract data)
+        {
+            var val = data.ActionMethod;
+
+            var session = System.Web.HttpContext.Current.Session;
+
+            if (session != null)
+            {
+                if (session["Values"] == null)
+                {
+                    session["Values"] = new string[] { "value1", "value2", "value3" };
+                    return  new string[] { "value1" }; 
+                }
+            }
+
+            //return new string[] { JsonConvert.SerializeObject((Object)session["Values"]) };
+            return new string[] { JsonConvert.SerializeObject((Object)session["Values"]) };
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
+
+public class LoginContract
+{
+    public string ActionMethod { get; set; }
+    public string StaffCode { get; set; }
+    public string Password { get; set; }
+}
